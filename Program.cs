d4538f318f9d00e7d0664ebebf85a3df4eed277d
QAEZZ <russian.devv@gmail.com>
2023-11-22 13:58:27 -0600

    init

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VCU
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine("arguments:");
+
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.WriteLine($"Argument: {args[i]}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No arguments provided.");
+            }
+        }
+    }
+}
